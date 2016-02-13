34e973e7dd53c1461cf97a8f18587bb756e64bdc
freeslugs <freeslugs@gmail.com>
2016-02-13 15:57:51 -0500

    init mdl

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 ## HammerTime Web App
 ### Features
 - Dashboard
+
 ### Install
 1. `git clone https://github.com/NWChen/Hammertime`
 2. `cd web`
 3. `pip install -r requirements.txt`
 4. `python app.py`
+
+### Misc
+#### Color Scheme:
+- blue grey: `#607D8B`
+- blue: `#2196F3`
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('layout.html')
 
 if __name__ == "__main__":
     app.run()
