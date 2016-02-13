0fe1b9e0e810877a30234f5cb15ff9e18739fbc3
Nick Liu <nl2523@columbia.edu>
2016-02-13 17:52:29 -0500

    UI now all working

--- Android/app/src/main/java/com/example/nl/hammertime/SetActive.java
+++ Android/app/src/main/java/com/example/nl/hammertime/SetActive.java
@@ -1,13 +1,28 @@
 package com.example.nl.hammertime;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
 
-public class SetActive extends AppCompatActivity {
+public class SetActive extends AppCompatActivity implements View.OnClickListener, SensorEventListener{
+
+    SensorManager sensorManager;
+    Sensor light;
+    ImageButton ibActiveBack;
+    TextView tvLight;
+    TextClock tcClock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +31,59 @@ public class SetActive extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ibActiveBack = (ImageButton) findViewById(R.id.ibActiveBack);
+        tvLight = (TextView) findViewById(R.id.tvLight);
+        tcClock = (TextClock) findViewById(R.id.tcClock);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.ibActiveBack:
+
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float ill = event.values[0];
+        boolean bPM = tcClock.getText().toString().substring(9,11).equals("PM") ? true : false;
+        String currentHourS = tcClock.getText().toString().substring(0, 2);
+        int currentHour = Integer.parseInt(currentHourS);
+        if (currentHourS.charAt(0) == '0')
+            currentHour = Integer.parseInt(currentHourS.substring(1,2));
+        if (bPM)
+            currentHour += 12;
+
+        String currentMinuteS = tcClock.getText().toString().substring(3, 5);
+        int currentMinute = Integer.parseInt(currentMinuteS);
+        if (currentMinuteS.charAt(0) == '0')
+            currentMinute = Integer.parseInt(currentMinuteS.substring(1,2));
+
+        if (currentHour > SetAlarm.hourChosen)
+            tvLight.setText("YES");
+        else if (currentHour == SetAlarm.hourChosen && currentMinute >= SetAlarm.minuteChosen)
+            tvLight.setText("YES");
+        else
+            tvLight.setText("NO");
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
--- Android/app/src/main/java/com/example/nl/hammertime/SetAlarm.java
+++ Android/app/src/main/java/com/example/nl/hammertime/SetAlarm.java
@@ -1,13 +1,30 @@
 package com.example.nl.hammertime;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
 
-public class SetAlarm extends AppCompatActivity {
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.PreparedStatement;
+
+public class SetAlarm extends AppCompatActivity implements View.OnClickListener {
+
+    TimePicker tpAlarmPicker;
+    ImageButton ibAlarmBack;
+    Button bConfirmAlarm;
+
+    public static int hourChosen, minuteChosen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +33,55 @@ public class SetAlarm extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        tpAlarmPicker = (TimePicker) findViewById(R.id.tpAlarmPicker);
+        ibAlarmBack = (ImageButton) findViewById(R.id.ibAlarmBack);
+        bConfirmAlarm = (Button) findViewById(R.id.bConfirmAlarm);
+
+        bConfirmAlarm.setOnClickListener(this);
+        ibAlarmBack.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.bConfirmAlarm:
+                //hourChosen = tpAlarmPicker.getBaseline();
+                //int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+                //if (currentApiVersion > android.os.Build.VERSION_CODES.LOLLIPOP_MR1){
+
+                String requestURL = "http://enterURLhere.com";
+                URL url;
+                HttpURLConnection conn;
+                try {
+                    url = new URL(requestURL);
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");
+                    conn.connect();
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("ID", "25");
+
+
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+
+                hourChosen = tpAlarmPicker.getHour();
+                minuteChosen = tpAlarmPicker.getMinute();
+
+                /*} else {
+                    PreparedStatement pstmt;
+                    hourChosen = tpAlarmPicker.getCurrentHour();
+                    minuteChosen = tpAlarmPicker.getCurrentMinute();
+                }*/
+                break;
+            case R.id.ibAlarmBack:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+    }
 }
