d1577adb5787129b014c9ae37ed7e7e6bdd9601c
Neil Chen <neilwchen@gmail.com>
2016-02-13 21:22:08 -0500

    revised POST receiver

--- pi/receiver.py
+++ pi/receiver.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
+import json
 
 my_ip = "160.39.166.246"
-start_time, end_time = 0, 0
-
+current_time, end_time = [], []
 
 # initialize app
 app = Flask(__name__)
@@ -10,13 +10,15 @@ app = Flask(__name__)
 # alarm settings route
 @app.route('/alarm_time', methods=['POST', 'GET'])
 def alarm_time():
-	payload = request.json	
-	print payload[start_time], payload[end_time] 
+	payload = request.json
+	print payload
+	
+	print payload['current_time'], payload['alarm_time'] 
 	return "Successful request."
 
 if __name__ == '__main__':
 	app.run(
 		host="0.0.0.0",
-		port=int("80")
+		port=int("8000")
 	)
 
