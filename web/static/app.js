447e336a78e160f594af5f0b8340a3bf8cee9f3d
freeslugs <freeslugs@gmail.com>
2016-02-13 23:05:21 -0500

    update js to ngrok

--- web/static/app.js
+++ web/static/app.js
@@ -1,4 +1,4 @@
-$.getJSON("http://localhost:8080/get_data").then(populateCharts)
+$.getJSON("http://hammertime.ngrok.com/get_data").then(populateCharts)
 
 function populateCharts(data) {
   var data = data.data;
